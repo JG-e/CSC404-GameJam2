94ee1a4347e6e22404ba12f39dc2d62f38a8ef74
JG-e <zezhengh@163.com>
2024-09-14 12:16:51 -0400

    Added new implementation for CharacterController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,26 +9,50 @@ public class PlayerController : MonoBehaviour
     public float rotateSpeed = 120.0f; 
     public float jumpForce = 5.0f; 
     private Rigidbody rb;
+
+    private CharacterController controller;
+    private Vector3 move_speed;
+    private int controllerFlag = 0; // 0 for rigidbody, 1 for character controller
+    
     // Start is called before the first frame update
+    // This script is responsible for the movement of the player either using rigidbody 
+    // or character controller
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        controllerFlag = rb == null ? 1 : 0;
+        if (controllerFlag == 1) {
+            controller = GetComponent<CharacterController>();
+        }
+        Debug.Log("controllerFlag: " + controllerFlag);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Jump")) {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+        if (controllerFlag == 0) {
+            if (Input.GetButtonDown("Jump")) {
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
         }
+        } else {
+            CharacterControllerMovement();
+        }
+
     }
 
     private void FixedUpdate() {
-        float moveVertical = Input.GetAxis("Vertical"); 
-        Vector3 movement = speed * moveVertical * transform.forward * Time.fixedDeltaTime;
-        rb.MovePosition(rb.position + movement);
-        float turn = Input.GetAxis("Horizontal") * rotateSpeed * Time.fixedDeltaTime;
-        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
-        rb.MoveRotation(rb.rotation * turnRotation);
+        if (controllerFlag == 0) {
+            float moveVertical = Input.GetAxis("Vertical"); 
+            Vector3 movement = speed * moveVertical * transform.forward * Time.fixedDeltaTime;
+            rb.MovePosition(rb.position + movement);
+            float turn = Input.GetAxis("Horizontal") * rotateSpeed * Time.fixedDeltaTime;
+            Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+            rb.MoveRotation(rb.rotation * turnRotation);
+        }
+    }
+
+    private void CharacterControllerMovement() {
+        move_speed=new Vector3(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed);
+        controller.SimpleMove(move_speed);     
     }
 }
